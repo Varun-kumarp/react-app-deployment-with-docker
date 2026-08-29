da2b9e79273e70055347080341bdf529d0ff9c09
root <root@ip-172-31-32-120.ap-south-2.compute.internal>
2026-08-29 11:41:46 +0000

    build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ docker stop react || true
 docker rm react || true
 
 # Build image
-docker build -t react-cicd:latest .
+docker build --no-cache -t react-cicd:latest .
 
 # Run container	
 docker run -d -it --name react -p 80:80 react-cicd:latest
@@ -23,3 +23,5 @@ docker push varun7560/react-cicd:v${BUILD_NUMBER}
 
 echo "============= Depolyment Completed =========="
 echo  "React version Deployed: v${BUILD_NUMBER}"
+
+docker ps
