234b7800de076a2821f601dc41f8a4ae035c5791
Varun-kumarp <95121153+Varun-kumarp@users.noreply.github.com>
2026-08-31 13:24:24 +0530

    Change heading from 'Most Used' to 'Most Famous'

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Most Used DevOps Tools In 2026</h2>
+          <h2>Most Famous DevOps Tools In 2026</h2>
 
         </div>
         <ul>
